24d6a281951f9a72e2f42cff0e1804b2c3d78763
CsuhanSuli <ps.gila456@gmail.com>
2025-11-02 23:26:42 +0100

    rosszmapa javitás

--- frontend/musicdbfront/Modules/awaitJson.js
+++
@@ -1,9 +0,0 @@
-export async function getJson(link)
-{
-    const response = await fetch(link)
-    if(!response.ok)
-        {
-            throw new Error("a csatlakozás sikertelen volt!!!");
-        }
-    return response.json();
-}
